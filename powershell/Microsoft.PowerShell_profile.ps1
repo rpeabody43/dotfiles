f684a1ff9462d6c681f20825f6ad23cc57e72cd6
Ryan Peabody <rpeabody43@gmail.com>
2023-08-18 00:27:48 -0400

    add terminal icons to pwsh profile

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,11 @@
+Import-Module -Name Terminal-Icons
 New-Alias which Get-Command
-oh-my-posh init pwsh --config 'C:/Users/rpeab/.dotfiles/powershell/bubblesextra.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'C:\Users\rpeab\.dotfiles\powershell\bubblesextra.omp.json' | Invoke-Expression
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
 # Be aware that if you are missing these lines from your profile, tab completion
 # for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
+# See https:\\ch0.co\tab-completion for details.
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
