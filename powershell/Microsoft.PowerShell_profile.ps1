81e52a9c0672302f40de761b0d1bd480f695cac4
Ryan Peabody <rpeabody43@gmail.com>
2023-08-17 22:59:21 -0400

    add choco to pwsh profile

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,11 @@
-oh-my-posh init pwsh --config '~/.dotfiles/powershell/bubblesextra.omp.json' | Invoke-Expression
+New-Alias which Get-Command
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/bubblesextra.omp.json' | Invoke-Expression
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
