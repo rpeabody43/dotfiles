ad93952d018420eb27f255575784869fe6516713
Ryan Peabody <rpeabody43@gmail.com>
2023-09-20 00:21:31 -0400

    set nerdtree to not show by default

--- nvim/lua/nerdtree.lua
+++ nvim/lua/nerdtree.lua
@@ -3,7 +3,6 @@ vim.cmd([[
     autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif
     " Toggle
     nnoremap <silent> <C-b> :NERDTreeToggle<CR>
-    autocmd VimEnter * NERDTree
 ]])
 
 vim.cmd("let NERDTreeMinimalUI=1")
